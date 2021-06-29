b34d735456f0f68f1929527225ea5cf9e705e2ae
madhannn <86282822+madhannn@users.noreply.github.com>
2021-06-30 00:15:07 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,5 +1,14 @@
+@setlocal enableextensions enabledelayedexpansion
 @echo off
-:check
-ping 127.0.0.1 > nul
-cls
-goto check
+set ipaddr=%1
+:loop
+set state=down
+for /f "tokens=5,6,7" %%a in ('ping -n 1 !ipaddr!') do (
+    if "x%%b"=="xunreachable." goto :endloop
+    if "x%%a"=="xReceived" if "x%%c"=="x1,"  set state=up
+)
+:endloop
+echo.Link is !state!
+ping -n 6 127.0.0.1 >nul: 2>nul:
+goto :loop
+endlocal

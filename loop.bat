e663d9793cad4a54f4306c296f2c20ee7b1257ed
madhannn <86282822+madhannn@users.noreply.github.com>
2021-06-30 00:19:21 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,14 +1,31 @@
-@setlocal enableextensions enabledelayedexpansion
 @echo off
-set ipaddr=%1
-:loop
-set state=down
-for /f "tokens=5,6,7" %%a in ('ping -n 1 !ipaddr!') do (
-    if "x%%b"=="xunreachable." goto :endloop
-    if "x%%a"=="xReceived" if "x%%c"=="x1,"  set state=up
-)
-:endloop
-echo.Link is !state!
-ping -n 6 127.0.0.1 >nul: 2>nul:
-goto :loop
-endlocal
+
+rem REPLACE PC NAME/IP ADDRESS BELOW WITH REQUIRED.
+set pc_name=192.168.1.1
+
+:start
+echo Please wait, testing if PC is online.
+cls
+ping -n 1 %pc_name% > nul
+if %errorlevel% == 0 goto run_command
+echo.
+echo PC is currently offline.
+echo.
+echo Waiting 120 seconds…
+ping -n 120 127.0.0.1 > nul
+goto start
+
+:run_command
+cls
+echo PC is online. Waiting 60 seconds for services to start.
+echo.
+ping -n 60 127.0.0.1 > nul
+echo.
+echo Running command…
+echo.
+
+REM COMMAND TO RUN GOES HERE.
+
+goto END
+
+:END pause
